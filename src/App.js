4fba66dd299ae16eb08e1f040f2d7b666440f9c0
samssjubair <samssjubair@gmail.com>
2021-05-29 11:56:26 +0600

    landing page section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import styled from 'styled-components'
 
 function App() {
   return (
     <div className="App">
-      <h1>Thunder motors</h1>
+      <Home/>
     </div>
   );
 }
