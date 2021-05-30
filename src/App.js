1c2fd024ea73e36ee018ef020f92c587347743cf
unknown <m.galib.rajjo15@gmail.com>
2021-05-30 15:01:00 +0600

    login implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Components/Home/Home';
-import styled from 'styled-components'
-import Header from './Components/Header/Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import styled from "styled-components";
+import Header from "./Components/Header/Header";
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <div className="App">
+        <Header />
+        <Home />
+      </div>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWtq7ZrVKtRNu_qk0iKqeSCDO4tg9wcE0",
+    authDomain: "thunder-car-4d3a1.firebaseapp.com",
+    projectId: "thunder-car-4d3a1",
+    storageBucket: "thunder-car-4d3a1.appspot.com",
+    messagingSenderId: "564543954141",
+    appId: "1:564543954141:web:e7fd46f0fd6d8b22d18f34"
+  };
+
+export default firebaseConfig;
